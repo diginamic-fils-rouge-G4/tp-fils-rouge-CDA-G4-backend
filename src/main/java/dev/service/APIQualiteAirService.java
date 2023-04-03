07331d0f5c188b6a8d4b4008007aaff01b880b4d
Mysteriou85 <guib5@hotmail.fr>
2023-04-03 18:22:04 +0200

    Modif de APIQualiteAirCtrl et service pour corriger le problème avec les favoris

--- src/main/java/dev/service/APIQualiteAirService.java
+++ src/main/java/dev/service/APIQualiteAirService.java
@@ -1,6 +1,7 @@
 package dev.service;
 
 import dev.entite.donneeApiQualiteAir.ApiResponse;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
@@ -21,8 +22,8 @@ public class APIQualiteAirService {
 
     /**
      * Récupère le feed d'une ville en fonction de son nom
-     * @param city
-     * @return
+     * @param city String
+     * @return String
      */
     public String getFeedCityByName(String city) {
         String url = "https://api.waqi.info/feed/" + city + "/?token=" + this.token_api;
@@ -31,11 +32,11 @@ public class APIQualiteAirService {
 
     /**
      * Récupère toutes les stations entre deux points de coordonnées
-     * @param lat1
-     * @param lng1
-     * @param lat2
-     * @param lng2
-     * @return
+     * @param lat1 String : latitude première coordonnée
+     * @param lng1 String : longitude première coordonnée
+     * @param lat2 String : latitude deuxième coordonnée
+     * @param lng2 String : longitude deuxième coordonnée
+     * @return String
      */
     public String getAllStations(String lat1, String lng1, String lat2, String lng2) {
         String url = "https://api.waqi.info/map/bounds?latlng=" +
@@ -46,9 +47,9 @@ public class APIQualiteAirService {
 
     /**
      * Récupère la station sur un point de coordonnée précis
-     * @param lat
-     * @param lng
-     * @return
+     * @param lat String : latitude de la coordonnée voulue
+     * @param lng String : longitude de la coordonnée voulue
+     * @return String
      */
     public String getMarkerByClick(String lat, String lng) {
         String url = "https://api.waqi.info/feed/geo:" + lat + ";" + lng + "/?token=" + this.token_api;
@@ -57,8 +58,8 @@ public class APIQualiteAirService {
 
     /**
      * Récupère une station grace à son nom
-     * @param station
-     * @return
+     * @param station String : nom de la station
+     * @return JSONObject
      */
     public JSONObject getStationByName(String station) {
         String url = "https://api.waqi.info/search/?keyword=" + station + "&token=" + this.token_api;
@@ -66,9 +67,19 @@ public class APIQualiteAirService {
     }
 
     /**
+     * Récupère une station en JSONObjet par rapport à son id
+     * @param id String : id de la station
+     * @return JSONObject
+     */
+    public JSONObject getStationByIdGetJSONObject(String id) {
+        String url = "https://api.waqi.info/feed/@" + id + "/?token=" + this.token_api;
+        return this.restTemplate.getForObject(url, JSONObject.class);
+    }
+
+    /**
      * Récupère une station par rapport à son id
-     * @param id
-     * @return
+     * @param id String : id de la station
+     * @return ApiResponse
      */
     public ApiResponse getStationById(String id) {
         String url = "https://api.waqi.info/feed/@" + id + "/?token=" + this.token_api;
